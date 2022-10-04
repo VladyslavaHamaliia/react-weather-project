9a38fc0c42de69226aa4952fef711622c5901a92
VladyslavaHamaliia <105849955+VladyslavaHamaliia@users.noreply.github.com>
2022-10-04 18:19:54 +0300

    added repo link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by Vladyslava Hamaliia and is
+        <a
+          href="https://github.com/VladyslavaHamaliia/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
