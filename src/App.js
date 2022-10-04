eb0e6e99678cfd9dc1c65602578dcd2c8a00a307
VladyslavaHamaliia <105849955+VladyslavaHamaliia@users.noreply.github.com>
2022-10-04 19:03:30 +0300

    added html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This project was coded by Vladyslava Hamaliia and is
-        <a
-          href="https://github.com/VladyslavaHamaliia/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        Weather App
+        <Weather />
+        <footer>
           {" "}
-          open-sourced
-        </a>
-      </footer>
+          This project was coded by Vladyslava Hamaliia and is
+          <a
+            href="https://github.com/VladyslavaHamaliia/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
